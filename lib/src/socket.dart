a0f61231cb948425520cf626fa49b1cb28ce638f
Pär Karlsson <pariz@MacBook-Pro-som-tillhor-Par.local>
2024-04-09 08:55:05 +0200

    Remove logging

--- lib/src/socket.dart
+++ lib/src/socket.dart
@@ -130,13 +130,8 @@ class Socket extends Emitter {
     if (message == "#1") {
       sendOrAdd('#2');
     } else {
-      // print message length
-      print('[Socket] Message received: ${message?.length}');
-
       // If the message is null or empty, we don't process it
       if (message == null || message.isEmpty) {
-        print(
-            '[Socket] Received empty message, this means we ack to not lose connection with the server');
         sendOrAdd("");
         return;
       }
